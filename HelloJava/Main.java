0d3e9f65e373b103c89e3e833e6ddaee71881873
llangit-broad <llangit@broadinstitute.org>
2020-09-02 23:53:48 +0000

    remove Java test files

--- HelloJava/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) 
-    {
-        System.out.println("Hello Java world");
-        System.out.println("This is a verification that Java is running properly");
-
-    }
-}
